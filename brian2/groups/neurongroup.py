15663dbafe81fb4ea82307298cfc40fc9941417b
Sagar Shahari <sagarshahari26@gmail.com>
2025-04-08 02:03:18 +0530

    Update neurongroup.py

--- brian2/groups/neurongroup.py
+++ brian2/groups/neurongroup.py
@@ -836,14 +836,35 @@ class NeuronGroup(Group, SpikeSource):
             else:
                 raise AssertionError(f"Unknown type of equation: {eq.eq_type}")
 
-        # Add the conditional-write attribute for variables with the
-        # "unless refractory" flag
-        if self._refractory is not False:
-            for eq in self.equations.values():
-                if eq.type == DIFFERENTIAL_EQUATION and "unless refractory" in eq.flags:
-                    not_refractory_var = self.variables["not_refractory"]
-                    var = self.variables[eq.varname]
-                    var.set_conditional_write(not_refractory_var)
+        # refractory variable setup
+    if 'spike' in self.events:
+        self.variables.add_array('_lastspike', size=self.N, dtype=float, constant=False, value=-1e100)
+
+    for event, refr in self._refractory.items():
+        event_name = event.replace(' ', '_')
+        self.variables.add_array(f'_lastevent_{event_name}', size=self.N, dtype=float, 
+                                constant=False, value=-1e100)
+        
+        if isinstance(refr, Quantity):
+            self.variables.add_array(f'_refractory_until_{event_name}', size=self.N, dtype=float, 
+                                    constant=False, value=-1e100)
+            self.variables.add_subexpression(f'not_refractory_{event_name}', 
+                                             f't >= _refractory_until_{event_name}')
+        elif isinstance(refr, str):
+            self.variables.add_subexpression(f'not_refractory_{event_name}', f'not ({refr})')
+            
+        
+        for eq in self.equations.values():
+            if eq.type == DIFFERENTIAL_EQUATION and "unless refractory" in eq.flags:
+                not_refractory_var = self.variables[f'not_refractory_{event_name}']
+                var = self.variables[eq.varname]
+                var.set_conditional_write(not_refractory_var)
+
+        # Events without refractory
+        for event in self.events:
+            event_name = event.replace(' ', '_')
+            if event not in self._refractory:
+                self.variables.add_subexpression(f'not_refractory_{event_name}', 'True')
 
         # Stochastic variables
         for xi in self.equations.stochastic_variables:
